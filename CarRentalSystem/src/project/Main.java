0dee0c6c4d4149378517e0dcd8b83eee905e36d1
Asad <asadmohammad8259@gmail.com>
2025-03-19 21:38:46 +0530

    second commit

--- CarRentalSystem/src/project/Main.java
+++ CarRentalSystem/src/project/Main.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Car 
-{
+class Car {
     private String carId;
     private String brand;
     private String model;
